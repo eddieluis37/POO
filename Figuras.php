a8aa192b8979d7555d459032d9a12da5730851c2
Eddie Rada <eddierada@gmail.com>
2017-05-19 10:30:13 -0500

    T8-1 Creación de estructura de jerarquia de clases y métodos abstractos

---
+++ Figuras.php
@@ -0,0 +1,20 @@
+<?php
+require 'Poligono.php';
+require 'Triangulo.php';
+require 'Cuadrado.php';
+require 'Rectangulo.php';
+require 'Hexagono.php';
+
+echo '
+    <h1>Polígonos</h1>
+    <p>Figura geometrica plana que esta limitada por tres o mas rectas y
+    tiene tres o más ángulos y vértices.</p>
+    <h2>Perimetro</h2>
+    <p>El perimetro de un poligono es igual a la suma de las longitudes de sus lados.</p>
+    <h2>el área de un poligono es la medida de la región o superfici encerrada por un poligono.</h2>
+    <hr>
+
+
+
+
+';
