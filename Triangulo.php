170c9f57ea13aace8f355a1db0600663b1978685
Eddie Rada <eddierada@gmail.com>
2017-05-30 15:22:36 -0500

    triangulo

---
+++ Triangulo.php
@@ -0,0 +1,15 @@
+<?php
+class Triangulo extends Poligono
+ {
+         private $lado_a;
+    private $lado_b;
+    private $lado_c;
+    private $altura;
+
+    public function __construct($a, $b, $c, $h)
+    {
+
+ }
+
+
+}
